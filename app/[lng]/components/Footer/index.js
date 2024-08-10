d25460fc50b4865164c4f76a220bd372cd268758
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-08-10 19:25:01 -0400

    Remove unused translation files and update npm dependencies

--- app/[lng]/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import { useTranslation } from 'react-i18next'
-import { FooterBase } from './FooterBase'
-
-export const Footer = async ({ lng }) => {
-  const { t } = await useTranslation(lng, 'footer')
-  return (
-    <FooterBase t={t} lng={lng} />
-  )
-}
